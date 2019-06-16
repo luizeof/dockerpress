1a6cff2bbeb7e6d29ecd55f5cc5519d9deefb582
Luiz Eduardo <luizeof@gmail.com>
2019-06-16 03:23:35 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 curl -o /var/www/wp-completion.bash https://raw.githubusercontent.com/wp-cli/wp-cli/v1.5.1/utils/wp-completion.bash
-
 echo 'source /var/www/wp-completion.bash' >> ~/.bash_profile
-
 source ~/.bash_profile
 
 service cron start 
-
 service cron reload
 
 exec /usr/local/bin/docker-entrypoint.sh "$@"
