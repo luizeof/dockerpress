bb4e5750ea53e4aa8fb2098eb6339dc89ccf8376
Luiz Eduardo <luizeof@gmail.com>
2019-06-24 20:11:46 -0300

    comandos

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-rm -f /var/www/wp-cli.phar
-
-curl -o /var/www/wp-cli.phar https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-
 mkdir -p $WP_CLI_CACHE_DIR
 chown -R www-data:www-data $WP_CLI_CACHE_DIR
-
 mkdir -p $WP_CLI_PACKAGES_DIR
 chown -R www-data:www-data $WP_CLI_PACKAGES_DIR
-
+rm -f /var/www/wp-cli.phar
+curl -o /var/www/wp-cli.phar https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x /var/www/wp-cli.phar
 
 if $(wp core is-installed); then
@@ -20,11 +16,9 @@ if $(wp core is-installed); then
     wp config set WP_REDIS_PORT $WP_REDIS_PORT --raw --add --type=constant
 
     rm -f /var/www/html/wp-content/object-cache.php
-
     wp plugin install redis-cache --force --activate
     wp redis enable
     wp redis update-dropin
-
     chmod +777 /var/www/html/wp-content/object-cache.php
 
 fi
