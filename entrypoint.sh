52a74ec5889d71fd369b98b6c128f41c4b84d792
Luiz Eduardo <luizeof@gmail.com>
2019-06-24 15:18:13 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-wpcli-setup
+if $(wp core is-installed); then
+
+    echo "Setting up Redis..."
+
+    wp config set WP_CACHE true --raw --add --type=constant
+    wp config set WP_REDIS_HOST $WP_REDIS_HOST --add --type=constant
+    wp config set WP_REDIS_DATABASE $WP_REDIS_DATABASE --raw --add --type=constant
+    wp config set WP_REDIS_PORT $WP_REDIS_PORT --raw --add --type=constant
+
+    wp plugin install redis-cache --force --activate
+    wp redis enable
+    wp redis update-dropin
+
+    chmod +777 /var/www/html/wp-content/advanced-cache.php
+
+    echo "Done."
+
+fi
 
 service cron start
 
