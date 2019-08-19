b188a63a96079152992d5be77bc30c5aa1a34437
Luiz Eduardo <luizeof@gmail.com>
2019-08-19 03:47:28 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -59,8 +59,11 @@ if [ ! -e wp-config.php ]; then
   			' wp-config-sample.php > wp-config.php <<'EOPHP'
   // If we're behind a proxy server and using HTTPS, we need to alert Wordpress of that fact
   // see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
-  if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
-  	$_SERVER['HTTPS'] = 'on';
+  if ($_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https')
+      $_SERVER['HTTPS'] = '1';
+
+  if (isset($_SERVER['HTTP_X_FORWARDED_HOST'])) {
+      $_SERVER['HTTP_HOST'] = $_SERVER['HTTP_X_FORWARDED_HOST'];
   }
 EOPHP
 
