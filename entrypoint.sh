acf15d448544c1cef7026e769a4b93cc2fe3dfb3
Luiz Eduardo <luizeof@gmail.com>
2019-11-18 14:06:51 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -62,7 +62,14 @@ if [ ! -e wp-config.php ]; then
   wp config shuffle-salts
 
   echo "Updating Database Info on wp-config.file "
+  wp config set WP_SITEURL "https://$VIRTUAL_HOST" --add --type=constant
+  wp config set WP_HOME "https://$VIRTUAL_HOST" --add --type=constant
+  wp config set DB_NAME $WORDPRESS_DB_NAME --add --type=constant
+  wp config set DB_USER $WORDPRESS_DB_USER --add --type=constant
+  wp config set DB_PASSWORD $WORDPRESS_DB_PASSWORD --add --type=constant
+  wp config set DB_HOST $WORDPRESS_DB_HOST --add --type=constant
   wp config set DB_PORT $WORDPRESS_DB_PORT --raw --add --type=constant
+  wp config set WP_DEBUG $WP_DEBUG --raw --add --type=constant
 
   # if Wordpress is installed
   if ! $(wp core is-installed); then
